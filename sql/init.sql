fc88e96488585ccd66f0b31b318859b7a1bdd29e
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-25 09:17:19 -0600

    Update init.sql script

--- sql/init.sql
+++ sql/init.sql
@@ -6,4 +6,14 @@ CREATE TABLE category (
     id INT IDENTITY PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
     description VARCHAR(255) NOT NULL
-)
+);
+
+INSERT INTO category (name, description) VALUES
+('Beverages', 'Soft drinks, coffees, teas, beers, and ales'),
+('Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings'),
+('Confections', 'Desserts, candies, and sweet breads'),
+('Dairy Products', 'Cheeses'),
+('Grains/Cereals', 'Breads, crackers, pasta, and cereal'),
+('Meat/Poultry', 'Prepared meats'),
+('Produce', 'Dried fruit and bean curd'),
+('Seafood', 'Seaweed and fish');
